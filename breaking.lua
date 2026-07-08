38831f3b013db64af38bfa0869b57ba9d4b8bf90
Dcheerria <anze9410@gmail.com>
2026-07-08 16:13:14 +0800

    Update breaking.lua

--- breaking.lua
+++ breaking.lua
@@ -1,1 +1,10 @@
-print("test")
+local character = game.Players.LocalPlayer.Character
+if character then
+    local tool = character:FindFirstChildWhichIsA("Tool")
+    if tool then
+        print("Tool:", tool.Name)
+        for _, v in ipairs(tool:GetDescendants()) do
+            print(v.ClassName, v.Name)
+        end
+    end
+end
