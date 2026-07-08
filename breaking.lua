d68afcf2055a554a48cb9ea5e51c7b7afacd027b
Dcheerria <anze9410@gmail.com>
2026-07-08 16:06:06 +0800

    Update breaking.lua

--- breaking.lua
+++ breaking.lua
@@ -1,11 +1,5 @@
--- iterate pelan-pelan biar gak trigger rate limit
 for _, v in ipairs(game:GetDescendants()) do
     if v:IsA("ModuleScript") then
-        local name = v.Name:lower()
-        if name:find("combat") or name:find("hit") or name:find("mine") 
-        or name:find("swing") or name:find("tool") or name:find("damage") then
-            print(v:GetFullName())
-            task.wait(0.1) -- delay antar request
-        end
+        print(v:GetFullName())
     end
 end
