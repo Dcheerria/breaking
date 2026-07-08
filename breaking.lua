1d9d6e01d1cec95ab1c8d9ef6b340f411585c8e8
Dcheerria <anze9410@gmail.com>
2026-07-08 16:10:25 +0800

    Update breaking.lua

--- breaking.lua
+++ breaking.lua
@@ -1,11 +1,1 @@
--- cek isi tool yang di-equip
-local character = game.Players.LocalPlayer.Character
-if character then
-    local tool = character:FindFirstChildWhichIsA("Tool")
-    if tool then
-        print("Tool:", tool.Name)
-        for _, v in ipairs(tool:GetDescendants()) do
-            print(" ", v.ClassName, v.Name)
-        end
-    end
-end
+print(game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Tool"))
